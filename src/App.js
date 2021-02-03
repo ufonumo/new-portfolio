580b4e0ad89ba56ee1a9ffae66613cc1b373fbc9
Ufonabasi Umo <31745515+ufonumo@users.noreply.github.com>
2021-02-03 23:21:53 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {BrowserRouter, Route} from "react-router-dom";
 import './App.css';
 import './bootstrap.min.css';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import NavbarTab from "./components/navbar";
 import About from './components/About';
 import Projects from "./components/Projects";
@@ -12,7 +12,7 @@ function App() {
     <BrowserRouter>
       <div className="App container">
         <NavbarTab/>
-
+ 
         <Route exact path='/Home' component={Home} />
         <Route  exact path="/About"  component={About} />
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,11 +3,9 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import {BrowserRouter as Router, Switch, Route,Link} from "react-router-dom";
 import logo from "../assets/Logo.svg";
-import vector from "../assets/Vector.svg";
-import photo from '../assets/Ellipse 1.svg';
-import scroll from "../assets/Scroll.svg";
+import Home from './Home';
 import About from './About';
-
+import Projects from "./Projects";
 
 const NavbarTab = () =>{
 
@@ -41,7 +39,7 @@ const NavbarTab = () =>{
                 <Nav className="ml-auto">
                   <Nav.Link > <Link to="/Home" className="nav-link link">Home</Link> </Nav.Link>
                   <Nav.Link > <Link to="/About" className="nav-link link">About Me</Link> </Nav.Link>
-                  <Nav.Link > <Link to="/UpdateOnstocks" className="nav-link link">Projects</Link> </Nav.Link>
+                  <Nav.Link > <Link to="/Projects" className="nav-link link">Projects</Link> </Nav.Link>
                   <Nav.Link > <Link to="/UpdateOnstocks" id='contact' className="nav-link link">Say Hello</Link> </Nav.Link>
                </Nav>
               
@@ -50,14 +48,10 @@ const NavbarTab = () =>{
 
         {/* HOME SECTION */}
 
+        <Home/>
+        <About/>
 
         {/* ABOUT ME SECTION */}
-        {/* <About/> */}
-
-        {/* SKILLS */}
-
-     
-
 
 
       </div>  
