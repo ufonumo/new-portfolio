7064efd1ab7fe3cf8dc663bcb75a56014b07db94
Ufonabasi Umo <31745515+ufonumo@users.noreply.github.com>
2021-02-06 01:27:51 +0100

    Add files via upload

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ export default function Home() {
                     <div className="col-lg-7 white">
                     <sub className='sub' id='home'>Hi there <img src={vector} alt="love"/></sub>
                     <p className='home_intro'>I’m <b>Ufonabasi Umo,</b> Frontend Developer, with a hands-on experience in creating web applications.</p>
-                    <a href="#"><button className='btn mt-3 mb-5'>View My Projects</button></a> 
+                    <a href="#projects"><button className='btn mt-3 mb-5'>View My Projects</button></a> 
                     </div>
                     <div className="col-lg-5 white mb-4">
                     <div className="blue_circle">
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,9 @@ import weaner from "../assets/weaners.jpg";
 import profile from "../assets/profile.jpg";
 import convert from "../assets/convert.jpg";
 import weather from "../assets/weather.jpg";
-import group from '../assets/Group.svg'
+import group from '../assets/Group.svg';
+import video from "../assets/stockapp.mp4";
+import HoverVideoPlayer from 'react-hover-video-player';
 
 export default function Projects() {
 
@@ -24,14 +26,16 @@ export default function Projects() {
             image: profile,
             alt: 'Food',
             description: 'orem ipsum dolor sit amet consectetur, adipisicing elit. Quo, recusandae? Laborum accusantium dol',
-            link: 'https://profile-search-ufon.netlify.app/'
-    
+            link: 'https://profile-search-ufon.netlify.app/',
+            code: `REACT ,  CSS ,  JAVASCRIPT`,
+
         },
     
         {
-            title: `Bookdown-FoodVendor `,
+            title: `BD-FoodVendor `,
             image: vendor,
             alt: 'Food',
+            code: `HTML ,  CSS , BOOTSTRAP, JAVASCRIPT `,
             description: 'orem ipsum dolor sit amet consectetur, adipisicing elit. Quo, recusandae? Laborum accusantium dol',
             link: 'https://food.bookdown.ng/vendor-app/signin.html'
     
@@ -41,6 +45,7 @@ export default function Projects() {
             title: `Degree Converter `,
             image: convert,
             alt: 'Food',
+            code: `REACT ,  CSS ,  JAVASCRIPT`,
             description: 'orem ipsum dolor sit amet consectetur, adipisicing elit. Quo, recusandae? Laborum accusantium dol',
             link: 'https://degree-minutes-converter.netlify.app/'
     
@@ -50,6 +55,7 @@ export default function Projects() {
             title: `Weaner's`,
             image: weaner,
             alt: 'Food',
+            code: `REACT ,  CSS ,  JAVASCRIPT`,
             description: 'orem ipsum dolor sit amet consectetur, adipisicing elit. Quo, recusandae? Laborum accusantium dol',
             link: 'https://gallant-hamilton-d2b950.netlify.app/'
     
@@ -60,6 +66,7 @@ export default function Projects() {
             title: `Weather Webapp`,
             image: weather,
             alt: 'Food',
+            code: `HTML ,  CSS , BOOTSTRAP, JAVASCRIPT `,
             description: 'orem ipsum dolor sit amet consectetur, adipisicing elit. Quo, recusandae? Laborum accusantium dol',
             link: 'https://weather-ufon.netlify.app/'
     
@@ -68,18 +75,27 @@ export default function Projects() {
 
     return (
         <div >
-            <section >
-             <h4 id="projects" className='colored_text'>FEATURED PROJECTS</h4>
+            <section id="projects">
+                <h4  className='colored_text pb-5'>FEATURED PROJECTS</h4>
 
                 <div className="row">
                     {projectData.map(list => (
                         <div className="col-lg-6 project mb-4">
-                            <img src={list.image} alt="project images"/>
+
+                            <HoverVideoPlayer
+                                videoSrc={video}
+                                pausedOverlay={
+                                    <img src={list.image} alt="video thumbnail" />
+                                }
+                                loadingOverlay={
+                                    <div className="loading-spinner-overlay" />
+                                }
+                            />
 
                             <div className="project_container">
                                 <div className='group_icon pt-4'>
                                     <h6>{list.title}</h6>
-                                <a href={list.link}> <img src={group} alt="link"/></a>
+                                    <a href={list.link}> <img src={group} alt="link"/></a>
                                 </div>
 
                                 <p>{list.description}</p>
