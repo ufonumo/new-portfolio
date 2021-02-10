a090dd0891f6e945c52e8809b362d14d6d22c416
ufonumo <ufonumo@gmail.com>
2021-02-10 17:28:34 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import {BrowserRouter, Route} from "react-router-dom";
 import './App.css';
 import './bootstrap.min.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavbarTab from "./components/navbar";
+import Loader from 'react-loader-spinner';
 
 
 function App() {
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000);
+  }, [])
+
   return (
     <BrowserRouter>
       <div className="App container">
-        <NavbarTab/>
+
+      {
+          loading ?
+           <Loader className='text-center spinner mt-5' type="Circles" color="#fff" height={80} width={80} />
+          :
+          <NavbarTab/>
+
+      }
       </div>
      </BrowserRouter>
   );
